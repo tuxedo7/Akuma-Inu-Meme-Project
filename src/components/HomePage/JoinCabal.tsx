b4e0fc57e70f35ab156c5c1b2223a9343ee1b4c7
TommyDev-Top <tommy.dev0127@gmail.com>
2025-02-07 21:50:11 -0800

    remove unused imported component

--- src/components/HomePage/JoinCabal.tsx
+++ src/components/HomePage/JoinCabal.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import { BuyWithoutFearInternal } from "./Team/BuyWithoutFearInternal";
 
 export const JoinCabal = () => {
   return (
